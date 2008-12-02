924f4807f66f584caa5aa4c5f1a213ce55eb9942
mikolalysenko <mikolalysenko@9aedf910-c000-11dd-919e-5d69a936ce07>
2008-12-02 23:41:32 +0000

    Linker error, need to fix it

--- trunk/volume_cuts.cpp
+++ trunk/volume_cuts.cpp
@@ -22,16 +22,16 @@ using namespace blitz;
 using namespace boost;
 
 //Includes from photohull
-extern const vec3 DN[], DU[], DV[];
-extern const int N_DIM[], U_DIM[], V_DIM[];
+extern const vec3 DN[6], DU[6], DV[6];
+extern const int N_DIM[6], U_DIM[6], V_DIM[6];
 
 //Lambda coefficient
 #define LAMBDA          0.1f
-#define INFINITY        1.0e20f
+#define INF             1.0e20f
 
 //Aliases for boost graph data type
 typedef adjacency_list_traits < vecS, vecS, directedS > Traits;
-typedef adjacency_list < vecS, vecS, directedS,
+typedef adjacency_list < vecS, vecS, directedS, no_property,
     property < edge_capacity_t, float,
     property < edge_residual_capacity_t, float,
     property < edge_reverse_t, Traits::edge_descriptor > > > > Graph;
@@ -42,18 +42,18 @@ Graph *g;
 
 //Graph property accessors
 property_map < Graph, edge_capacity_t >::type
-    capacity = get(edge_capacity, g);
+    capacity = get(edge_capacity, *g);
 property_map < Graph, edge_reverse_t >::type
-    rev = get(edge_reverse, g);
+    rev = get(edge_reverse, *g);
 property_map < Graph, edge_residual_capacity_t >::type
-    residual_capacity = get(edge_residual_capacity, g);
+    residual_capacity = get(edge_residual_capacity, *g);
 
 
 //Volume dimensions
 ivec3 grid_dim;
 
 //Node coordinates
-int source, sink;
+int sourceNode, sinkNode;
 int getNode(int x, int y, int z)
 {
     assert(x >= 0 && x < grid_dim(0) &&
@@ -64,23 +64,39 @@ int getNode(int x, int y, int z)
 }
 
 //Evaluate photoconsistency of a given grid point
-float evaluatePhotoConsistency(vector<View*> views, vec4 p, Volume * hull)
+float evaluatePhotoConsistency(vector<View*> views, vec3 p, Volume * hull)
 {
     return 1.0f;
 }
 
 void addEdge(int start, int end, float w)
 {
-    for(Graph::out_edge_iterator it = 
+    Traits::vertex_descriptor u, v;
+    u = vertex(start, *g);
+    v = vertex(end, *g);
+ 
+    //Check for edge existence
+    Traits::edge_descriptor e1, e2;
+    bool found;
+    tie(e1, found) = edge(u, v, *g);
+    if(found)
+        return;
     
-    graph_traits<Graph>::edge_descriptor 
-        e0 = add_edge(start, end, *g),
-        e1 = add_edge(end, start, *g);
+    //Add edge to graph
+    add_edge(u, v, *g);
+    add_edge(v, u, *g);
     
-    capacity[e0] = capacity[e1] = w;
-    residual_capacity[e0] = residual_capacity[e1] = w;
-    rev[e0] = e1;
-    rev[e1] = e0;
+    //Locate edges
+    tie(e1, found) = edge(u, v, *g);
+    tie(e2, found) = edge(v, u, *g);
+    
+    //Set properties
+    capacity[e1] = w;
+    capacity[e2] = w;
+    residual_capacity[e1] = w;
+    residual_capacity[e2] = w;
+    rev[e2] = e1;
+    rev[e1] = e1;
 }
     
 
@@ -110,13 +126,13 @@ Volume* volumetricGraphCuts(
     ivec3 interior_grid;
     for(int i=0; i<3; i++)
     {
-        interior_gird(i) = 
+        interior_grid(i) = 
             grid_res(i) * (interior_point(i) - box_min(i)) / (box_max(i) - box_min(i));
     }
-    source = getNode(interior_grid(0), interior_grid(1), interior_grid(2));
+    sourceNode = getNode(interior_grid(0), interior_grid(1), interior_grid(2));
     
     //Find sink
-    sink = -1;
+    sinkNode = -1;
     
     //Build graph
     for(int i=0; i<grid_dim(0); i++)
@@ -126,12 +142,12 @@ Volume* volumetricGraphCuts(
         int v = getNode(i, j, k);
         
         //Check for edge cells
-        if(photo_hull->on_surface(i, j, k))
+        if(photo_hull->on_surface(ivec3(i, j, k)))
         {
-            if(sink == -1)
-                sink = v;
+            if(sinkNode == -1)
+                sinkNode = v;
             else
-                addEdge(v, sink, INFINITY);
+                addEdge(v, sinkNode, INF);
             
             continue;
         }
@@ -143,20 +159,20 @@ Volume* volumetricGraphCuts(
             pt *= 0.5;
             pt += vec3(i,j,k);
             
-            float rho = evaluatePhotoConsistency(views, pt, hull);
+            float rho = evaluatePhotoConsistency(views, pt, photo_hull);
             
             int u = getNode(i+DN[d](0), j+DN[d](1), k+DN[d](2));
             
-            float weight = 4.0f / 3.0f * M_PI * h(U_DIMS[d]) * h(V_DIMS[d]) * rho;
+            float weight = 4.0f / 3.0f * M_PI * h(U_DIM[d]) * h(V_DIM[d]) * rho;
             addEdge(v, u, weight);
         }
         
         
         //Add inflation term
-        if(v != SOURCE)
+        if(v != sourceNode)
         {
             float w_b = LAMBDA * h(0) * h(1) * h(2);
-            addEdge(v, source, w_b);
+            addEdge(v, sourceNode, w_b);
         }
     }
     
@@ -167,10 +183,10 @@ Volume* volumetricGraphCuts(
     vector<Traits::edge_descriptor> pred(num_vertices(*g));
     
     //Do min-cuts on graph to get result
-    float flow = edmunds_karp_max_flow(
+    edmunds_karp_max_flow(
         *g, 
-        source, 
-        sink, 
+        sourceNode, 
+        sinkNode, 
         capacity, 
         residual_capacity, 
         rev,
